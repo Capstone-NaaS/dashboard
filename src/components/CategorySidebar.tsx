65baf1ed3840948f1df94efef46d0d51036c1a23
Erin <rosemary.erin.o@gmail.com>
2024-11-06 11:18:35 -0500

    refactor: updates navigation to use useNavigate hook and programmatically generate Sidebar.Items

--- src/components/CategorySidebar.tsx
+++ src/components/CategorySidebar.tsx
@@ -1,25 +1,81 @@
-// https://flowbite-react.com/docs/components/sidebar
 import { Sidebar } from "flowbite-react";
 import { HiArrowSmRight, HiTable, HiUser } from "react-icons/hi";
 import { FaChartLine } from "react-icons/fa6";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function CategorySidebar() {
+  /*
+  The notification logs should be the default active category.
+  When the active category is set navigate should be called
+  */
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const [activeCategory, setActiveCategory] = useState("");
+
+  const CATEGORIES = [
+    {
+      name: "Notification Logs",
+      icon: HiTable,
+      path: "notification-logs",
+    },
+    {
+      name: "Users",
+      icon: HiUser,
+      path: "users",
+    },
+    {
+      name: "Analytics",
+      icon: FaChartLine,
+      path: "analytics",
+    },
+    {
+      name: "Sign In",
+      icon: HiArrowSmRight,
+      path: "sign-in",
+    },
+  ];
+  const ACTIVE_CLASS = "border-b border-gray-500";
+  const INACTIVE_CLASS = "";
+
+  // on mount set notification logs as the active category and set
+  // notification-logs as entrypoint
+  useEffect(() => {
+    setActiveCategory("notification-logs");
+    navigate("notification-logs");
+  }, []);
+
+  const handleClick = (event: React.MouseEvent) => {
+    // set active category to clicked category
+    const categoryValue = event.target.textContent
+      .toLowerCase()
+      .replace(" ", "-");
+    console.log({ categoryValue });
+    setActiveCategory(categoryValue);
+    navigate(categoryValue);
+  };
+
   return (
     <Sidebar className="w-1/5" aria-label="Default sidebar example">
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-          <Sidebar.Item active={true} href="#" icon={HiTable}>
-            Notification Logs
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiUser}>
-            My Users
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={FaChartLine}>
-            Analytics
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiArrowSmRight}>
-            Sign In
-          </Sidebar.Item>
+          {CATEGORIES.map((category) => {
+            return (
+              <Sidebar.Item
+                icon={category.icon}
+                onClick={handleClick}
+                className={`${
+                  activeCategory === category.path
+                    ? ACTIVE_CLASS
+                    : INACTIVE_CLASS
+                }`}
+                active={activeCategory === category.path} // honestly not sure if this is necessary
+              >
+                {category.name}
+              </Sidebar.Item>
+            );
+          })}
         </Sidebar.ItemGroup>
       </Sidebar.Items>
     </Sidebar>

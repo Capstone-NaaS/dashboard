e9617c596a8af6d0df462d2083e6b0c0628fdba8
Erin <rosemary.erin.o@gmail.com>
2024-11-06 11:12:43 -0500

    refactor: reorganize app component to display general selected window component and info window component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import CategorySidebar from "./components/CategorySidebar";
-import LogsSidebar from "./components/LogsSidebar";
-import LogTable from "./components/LogTable";
-import LogDetailsSidebar from "./components/LogDetailsSidebar";
+import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
+import CategorySidebar from "./components/CategorySidebar";
+import SelectedWindow from "./components/SelectedWindow";
+import InfoWindow from "./components/InfoWindow";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="flex h-[calc(100vh-6rem)] items-start justify-center gap-2">
         <CategorySidebar />
-        <LogsSidebar />
-        <LogDetailsSidebar />
-        {/* <LogTable /> */}
+        <SelectedWindow></SelectedWindow>
+        <InfoWindow></InfoWindow>
       </main>
-    </>
+    </Router>
   );
 }
 

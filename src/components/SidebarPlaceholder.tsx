1efd8d87f6376a7a7602de0ed2f63a18bca22acb
Erin <rosemary.erin.o@gmail.com>
2024-11-07 18:23:12 -0500

    chore: add placeholder component for development

---
+++ src/components/SidebarPlaceholder.tsx
@@ -0,0 +1,12 @@
+import { Sidebar } from "flowbite-react";
+function Placeholder() {
+  return (
+    <Sidebar className="w-4/5" aria-label="Default sidebar example">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup></Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
+
+export default Placeholder;

9ba82437001530c6fb8029e9e8ebe40275321b8c
Erin <rosemary.erin.o@gmail.com>
2024-11-11 14:45:14 -0500

    feat: fetch all users for dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -8,32 +8,6 @@ import BackendSDK from "./../../backend-sdk/src/index.ts";
 
 const apiUrl = import.meta.env.VITE_HTTP_GATEWAY;
 const naas = new BackendSDK("secretkey1", apiUrl!);
-const MOCK_USERS = [
-  {
-    id: 1,
-    name: "Erin Olson",
-    email: "erin@mail.com",
-    created_at: "Oct 14, 06:24 PM",
-    last_seen: "Nov 2, 11:34 AM",
-    last_notified: "Nov 2, 11:34 AM",
-  },
-  {
-    id: 2,
-    name: "Clover Davidson",
-    email: "iluvsnacks@mail.com",
-    created_at: "Oct 14, 06:24 PM",
-    last_seen: "Nov 2, 11:34 AM",
-    last_notified: "Nov 2, 11:34 AM",
-  },
-  {
-    id: 3,
-    name: "Aster Davidson",
-    email: "runsleepsnack@mail.com",
-    created_at: "Oct 14, 06:24 PM",
-    last_seen: "Nov 2, 11:34 AM",
-    last_notified: "Nov 2, 11:34 AM",
-  },
-];
 
 function App() {
   const [logs, setLogs] = useState([]);
@@ -53,9 +27,8 @@ function App() {
     const fetchUsers = async () => {
       try {
         console.log("fetching users...");
-        const fetchedUsers = await naas.getUser("13");
-        //setUsers([fetchedUsers.message]);
-        setUsers(MOCK_USERS);
+        const fetchedUsers = await naas.getAllUsers();
+        setUsers(fetchedUsers.message);
       } catch (error) {
         console.error("Error fetching users: ", error);
       }

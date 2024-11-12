f7ae59e798a8973114830ea0a04638140b587b42
Erin <rosemary.erin.o@gmail.com>
2024-11-12 13:25:10 -0500

    feat: display only most recent log for a given notification and order table results from most to least recently created

--- src/components/LogsTable.tsx
+++ src/components/LogsTable.tsx
@@ -1,6 +1,60 @@
 import { Table, Badge } from "flowbite-react";
 
 function LogsTable({ logs }) {
+  function sortAndFilterLogs(logs) {
+    logs = JSON.parse(JSON.stringify(logs)); // deep copy
+
+    // sort the logs to group by created_at most recent to least recent log
+    logs.sort((logA, logB) => {
+      const dateA = new Date(logA.created_at);
+      const dateB = new Date(logB.created_at);
+
+      if (dateA > dateB) {
+        // date A came after date B
+        return -1;
+      } else if (dateA < dateB) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    let mostRecent = [];
+    let prevNotifId;
+
+    for (let index = 0; index < logs.length; index++) {
+      let log = logs[index];
+
+      if (log.notification_id === prevNotifId) {
+        continue;
+      }
+
+      // if there is no log with a matching notification_id, add to most recent array
+      let match = !!mostRecent.find(
+        (obj) => obj.notification_id === log.notification_id
+      );
+
+      if (!match) {
+        mostRecent.push(log);
+        prevNotifId = log.notification_id;
+      }
+    }
+
+    return mostRecent;
+  }
+
+  function formatDate(isoString) {
+    const date = new Date(isoString);
+
+    const formattedDate = date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    });
+
+    return formattedDate;
+  }
+
   function getBadge(log) {
     let badgeColor;
 
@@ -18,6 +72,7 @@ function LogsTable({ logs }) {
       </div>
     );
   }
+  const tableData = sortAndFilterLogs(logs);
 
   return (
     <div className="overflow-x-auto flex-grow">
@@ -30,7 +85,7 @@ function LogsTable({ logs }) {
           <Table.HeadCell>Date</Table.HeadCell>
         </Table.Head>
         <Table.Body className="divide-y">
-          {logs.map((log) => {
+          {tableData.map((log) => {
             return (
               <Table.Row
                 key={log.log_id}
@@ -42,7 +97,7 @@ function LogsTable({ logs }) {
                 <Table.Cell>User {log.user_id}</Table.Cell>
                 <Table.Cell>{log.channel}</Table.Cell>
                 <Table.Cell>{log.message}</Table.Cell>
-                <Table.Cell>{log.created_at}</Table.Cell>
+                <Table.Cell>{formatDate(log.created_at)}</Table.Cell>
                 <Table.Cell>
                   <a href="#" className="font-medium hover:underline">
                     Log Details

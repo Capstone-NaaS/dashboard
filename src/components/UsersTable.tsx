2e51c78594f22f6341a122ac5c4a0de22539d7a6
Kwang Lee <kwjolee@gmail.com>
2024-11-14 16:25:47 -0500

    feat: adding more filtering and sorting to users table

--- src/components/UsersTable.tsx
+++ src/components/UsersTable.tsx
@@ -4,6 +4,9 @@ import { useSearchParams } from "react-router-dom";
 import { Table, TextInput, Label, Spinner } from "flowbite-react";
 import formatDate from "../utils/formatDate";
 import { User } from "../types/index";
+import { CiBellOn } from "react-icons/ci";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaSortUp, FaSortDown } from "react-icons/fa";
 
 function UsersTable() {
   const [searchParams] = useSearchParams();
@@ -15,6 +18,10 @@ function UsersTable() {
   const [loading, setLoading] = useState(true);
   const [idFilter, setIdFilter] = useState(initialIdFilter);
   const [emailFilter, setEmailFilter] = useState(initialEmailFilter);
+  const [inAppFilter, setInAppFilter] = useState(true);
+  const [emailPrefFilter, setEmailPrefFilter] = useState(true);
+  const [sortColumn, setSortColumn] = useState<string | null>(null);
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc" | null>(null);
 
   useEffect(() => {
     const getAllUsers = async () => {
@@ -53,10 +60,39 @@ function UsersTable() {
       const matchesEmail = user.email
         .toLowerCase()
         .includes(emailFilter.toLowerCase());
-      return matchesId && matchesEmail;
+      const matchesInAppPref = user.preferences.in_app === inAppFilter;
+      const matchesEmailPref = user.preferences.email === emailPrefFilter;
+
+      return matchesId && matchesEmail && matchesInAppPref && matchesEmailPref;
     });
     setFilteredUsers(newFilteredUsers);
-  }, [users, idFilter, emailFilter]);
+  }, [users, idFilter, emailFilter, inAppFilter, emailPrefFilter]);
+
+  const handleSort = (column: "created_at" | "last_seen" | "last_notified") => {
+    let newSortOrder: "asc" | "desc" | null;
+    if (sortColumn === column) {
+      newSortOrder =
+        sortOrder === "asc" ? "desc" : sortOrder === "desc" ? null : "asc";
+    } else {
+      newSortOrder = "asc";
+    }
+
+    setSortColumn(column);
+    setSortOrder(newSortOrder);
+
+    if (newSortOrder) {
+      setFilteredUsers((prevUsers) =>
+        [...prevUsers].sort((a, b) => {
+          const aVal = a[column] || "";
+          const bVal = b[column] || "";
+          if (newSortOrder === "asc") return aVal > bVal ? 1 : -1;
+          return aVal < bVal ? 1 : -1;
+        })
+      );
+    } else {
+      setFilteredUsers([...users]);
+    }
+  };
 
   return (
     <div className="overflow-x-auto w-full">
@@ -79,7 +115,25 @@ function UsersTable() {
             onChange={(e) => setEmailFilter(e.target.value)}
           />
         </div>
+        <div className="flex flex-col">
+          <Label className="mb-2">Filter by Preferences</Label>
+          <div className="flex space-x-2">
+            <CiBellOn
+              size={24}
+              color={inAppFilter ? "#1E90FF" : "#B0C4DE"}
+              onClick={() => setInAppFilter(!inAppFilter)}
+              className="cursor-pointer"
+            />
+            <MdOutlineEmail
+              size={24}
+              color={emailPrefFilter ? "#1E90FF" : "#B0C4DE"}
+              onClick={() => setEmailPrefFilter(!emailPrefFilter)}
+              className="cursor-pointer"
+            />
+          </div>
+        </div>
       </div>
+
       <div className="overflow-x-auto flex-grow">
         {loading ? (
           <div className="flex justify-center items-center p-8">
@@ -89,11 +143,54 @@ function UsersTable() {
         ) : (
           <Table>
             <Table.Head>
+              <Table.HeadCell>User ID</Table.HeadCell>
               <Table.HeadCell>Name</Table.HeadCell>
               <Table.HeadCell>Email</Table.HeadCell>
-              <Table.HeadCell>Created At</Table.HeadCell>
-              <Table.HeadCell>Last Seen</Table.HeadCell>
-              <Table.HeadCell>Last Notified</Table.HeadCell>
+              <Table.HeadCell
+                onClick={() => handleSort("created_at")}
+                className="cursor-pointer"
+              >
+                <div className="flex items-center">
+                  Created At{" "}
+                  {sortColumn === "created_at" &&
+                    (sortOrder === "asc" ? (
+                      <FaSortUp className="ml-2" />
+                    ) : sortOrder === "desc" ? (
+                      <FaSortDown className="ml-2" />
+                    ) : null)}
+                </div>
+              </Table.HeadCell>
+
+              <Table.HeadCell
+                onClick={() => handleSort("last_seen")}
+                className="cursor-pointer"
+              >
+                <div className="flex items-center">
+                  Last Seen{" "}
+                  {sortColumn === "last_seen" &&
+                    (sortOrder === "asc" ? (
+                      <FaSortUp className="ml-2" />
+                    ) : sortOrder === "desc" ? (
+                      <FaSortDown className="ml-2" />
+                    ) : null)}
+                </div>
+              </Table.HeadCell>
+
+              <Table.HeadCell
+                onClick={() => handleSort("last_notified")}
+                className="cursor-pointer"
+              >
+                <div className="flex items-center">
+                  Last Notified{" "}
+                  {sortColumn === "last_notified" &&
+                    (sortOrder === "asc" ? (
+                      <FaSortUp className="ml-2" />
+                    ) : sortOrder === "desc" ? (
+                      <FaSortDown className="ml-2" />
+                    ) : null)}
+                </div>
+              </Table.HeadCell>
+              <Table.HeadCell>Preferences</Table.HeadCell>
             </Table.Head>
             <Table.Body className="divide-y">
               {filteredUsers.length > 0 ? (
@@ -102,17 +199,32 @@ function UsersTable() {
                     key={user.id}
                     className="bg-white dark:border-gray-700 dark:bg-gray-800"
                   >
+                    <Table.Cell>{user.id}</Table.Cell>
                     <Table.Cell>{user.name}</Table.Cell>
                     <Table.Cell>{user.email}</Table.Cell>
                     <Table.Cell>{formatDate(user.created_at)}</Table.Cell>
                     <Table.Cell>{formatDate(user.last_seen)}</Table.Cell>
                     <Table.Cell>{formatDate(user.last_notified)}</Table.Cell>
+                    <Table.Cell>
+                      <div className="flex space-x-2">
+                        <CiBellOn
+                          size={24}
+                          color={
+                            user.preferences.in_app ? "#1E90FF" : "#B0C4DE"
+                          }
+                        />
+                        <MdOutlineEmail
+                          size={24}
+                          color={user.preferences.email ? "#1E90FF" : "#B0C4DE"}
+                        />
+                      </div>
+                    </Table.Cell>
                   </Table.Row>
                 ))
               ) : (
                 <Table.Row>
                   <Table.Cell
-                    colSpan={5}
+                    colSpan={6}
                     className="text-center p-4 text-gray-500"
                   >
                     No users to display.
